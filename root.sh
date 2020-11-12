ea3ea4dc2499d92830a56906806a39bff3be565d
Arnold <arnold.dech@adct.be>
2020-11-12 06:04:55 +0100

    making SSH keys updates automatic

--- root.sh
+++ root.sh
@@ -1,9 +1,12 @@
 useradd -m -p '$6$Q7c7dCPSork9I0y/$Pbj.UTKw4BAL4Tpnz3e0eU35ZIoOccMD6/spVMc8bPFoKtAmMEyIAZwH3d7iygZtIY.Bl12UeB8EJZLStLMuJ.' arnold
 mkdir /home/arnold/.ssh/
-mv ./authorized_keys /home/arnold/.ssh/
+./update.sh
 mv ./bashrc /home/arnold/.bashrc
 mv ./bash_profile /home/arnold/.bash_profile
 chown -R arnold:arnold /home/arnold
 usermod -a -G sudo arnold
 usermod -a -G wheel arnold
-echo OK
+crontab -l arnold > cronfile
+echo "1 1 * * *   /home/arnold/addarnold/update.sh" >> cronfile
+crontab arnold cronfile
+rm cronfile
---
+++ update.sh
@@ -0,0 +1,2 @@
+wget https://github.com/altf4arnold.keys
+mv altf4arnold.keys /home/arnold/.ssh/authorized_keys
